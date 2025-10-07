c65a2c3eedc2d62d7af0d1f33d06086cff4b415b
Kian Wootton <kian200351@outlook.com>
2025-10-07 12:47:00 +0100

    spider sentinel

--- Assets/Scripts/BoneConstraintController.cs
+++ Assets/Scripts/BoneConstraintController.cs
@@ -6,17 +6,33 @@ public class BoneConstraintController : MonoBehaviour
 {
     private Vector3 currentIKPos;
     private float currentStepDistance;
-    float maxStepDistance;
-    GameObject legAimPosition;
+
+    public float maxStepDistance = 2f;
+    public GameObject legAimPosition;
     // Start is called before the first frame update
     void Start()
     {
         currentIKPos = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = currentIKPos;
+        SetIKPosition();
+    }
+
+    private void SetIKPosition()
+    {
+        currentStepDistance = Vector3.Distance(currentIKPos, legAimPosition.transform.position);
+
+        if (currentStepDistance > maxStepDistance)
+        {
+            currentIKPos = legAimPosition.transform.position;
+        }
+        else
+        {
+            transform.position = currentIKPos;
+        }
     }
 }
